f5beceec93002a765f5af0cdc6de6b83dc6a9b58
Paweł Szopiński <szopinskipawel@gmail.com>
2023-03-24 22:51:58 +0100

    Gallery structure created

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector(".gallery");
+for (let i = 0; i < galleryItems.length; i++) {
+  const imageArr = galleryItems[i];
 
-console.log(galleryItems);
+  const markup = `<div class="gallery__item">
+  <a class="gallery__link" href=javascript:void(0)>
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source=${imageArr.original}
+      alt="${imageArr.description}"
+    />
+  </a>
+</div>`;
+  list.insertAdjacentHTML("beforeend", markup);
+}
